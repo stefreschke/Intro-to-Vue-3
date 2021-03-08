89df587692616bf5fe65c5617a7384339e8610f5
Stefan Reschke <stefan.reschke@outlook.de>
2021-03-08 15:30:16 +0100

    done section3

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: "./assets/images/socks_blue.jpg",
+            myUrl: "http://google.com/"
         }
     }
 })
